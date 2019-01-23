96ab0e1f0a94c03bb0bbdc20989a711252de9b21
carlos.reyes <carlos.reyes@MU-C1-18.mullen.pvt.k12.co.us>
2019-01-23 10:10:58 -0700

    Added in those file acceptors

--- Group Creator/src/Creator.java
+++ Group Creator/src/Creator.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.File;
 
 public class Creator
 	{
-		public static void main(String[] args)
+		public static void main(String[] args) throws IOException
 		{
 			
 		}
