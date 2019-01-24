5cf3956adc6998b114b574ef994dedc04ee2d504
carlos.reyes <carlos.reyes@MU-C1-18.mullen.pvt.k12.co.us>
2019-01-24 08:29:43 -0700

    ok idk what is happening

--- Group Creator/src/Creator.java
+++ Group Creator/src/Creator.java
@@ -6,6 +6,31 @@ public class Creator
 	{
 		public static void main(String[] args) throws IOException
 		{
+			studentNames();
+			something2();
+			something3();
 			
 		}
+
+		private static void studentNames()
+			{
+		//		Scanner names = new Scanner(new File("Random22Names.text"));
+			
+				
+			}
+
+		private static void something2()
+			{
+				
+				System.out.println();
+				System.out.println();
+				System.out.println();System.out.println();
+				
+			}
+
+		private static void something3()
+			{
+				
+				
+			}
 	}
