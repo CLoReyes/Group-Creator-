44338a0efe4ec22f4914d0e20d973c2378035365
carlos.reyes <carlos.reyes@MU-C1-18.mullen.pvt.k12.co.us>
2019-01-24 09:11:35 -0700

    finally you start

--- Group Creator/src/Creator.java
+++ Group Creator/src/Creator.java
@@ -7,30 +7,26 @@ public class Creator
 		public static void main(String[] args) throws IOException
 		{
 			studentNames();
-			something2();
-			something3();
-			
+	
+			groupGenerator();
+			//collections/shuffle(myArray)
 		}
 
 		private static void studentNames()
 			{
-		//		Scanner names = new Scanner(new File("Random22Names.text"));
 			
 				
 			}
 
-		private static void something2()
-			{
-				
-				System.out.println();
-				System.out.println();
-				System.out.println();System.out.println();
-				
-			}
 
-		private static void something3()
+		private static void groupGenerator()
 			{
-				
+				boolean restart = true;
+				while (restart)
+					{
+						Scanner options = new Scanner (System.in);
+						System.out.println("How many groups would you like?");
+					}
 				
 			}
 	}
